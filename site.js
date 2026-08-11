d05f35c9076a2a9d2560ff3fee53e068294bfe9a
Christopher Felder <felderc@usf.edu>
2026-08-11 16:42:31 -0400

    Update site.js

--- site.js
+++ site.js
@@ -1,12 +1,13 @@
-const menuButton = document.querySelector(".menu-button");
-const navigation = document.querySelector(".nav-links");
+const b = document.querySelector(".menu-button");
+const n = document.querySelector(".nav-links");
 
-if (menuButton && navigation) {
-  menuButton.addEventListener("click", () => {
-    navigation.classList.toggle("open");
+if (b && n) {
+  b.addEventListener("click", () => {
+    n.classList.toggle("open");
 
-    const isOpen = navigation.classList.contains("open");
-
-    menuButton.setAttribute("aria-expanded", isOpen);
+    b.setAttribute(
+      "aria-expanded",
+      n.classList.contains("open")
+    );
   });
 }
